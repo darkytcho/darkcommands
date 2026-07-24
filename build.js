fe7e6166b11ddd9c9ade545e20f474409a9b8df1
Vítor Sousa <vitortls93@gmail.com>
2026-07-24 01:46:05 -0300

    fix: simplificar build obf - remover opcoes agressivas que quebravam o script

--- build.js
+++ build.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 const Terser = require('terser');
 
-function xorEncode(str, key) {
-    let result = '';
-    for (let i = 0; i < str.length; i++) {
-        result += String.fromCharCode(str.charCodeAt(i) ^ key.charCodeAt(i % key.length));
-    }
-    return result;
-}
-
 async function main() {
     const src = fs.readFileSync('Dark Commands.user.js', 'utf8');
 
@@ -16,15 +8,20 @@ async function main() {
     const code = metaMatch ? src.slice(metaMatch[1].length) : src;
     const meta = metaMatch ? metaMatch[1] : '';
 
-    const minified = await Terser.minify(code, { compress: true, mangle: true });
-    if (minified.error) throw minified.error;
+    const result = await Terser.minify(code, {
+        compress: {
+            passes: 2,
+            drop_debugger: true,
+            collapse_vars: true,
+            reduce_vars: true
+        },
+        mangle: true,
+        output: { comments: false }
+    });
 
-    const key = 'd@rk' + Math.random().toString(36).slice(2, 6);
-    const encoded = Buffer.from(xorEncode(minified.code, key)).toString('base64');
+    if (result.error) throw result.error;
 
-    const loader = `(function(){var k='${key}',d='',s=atob('${encoded}');for(var i=0;i<s.length;i++)d+=String.fromCharCode(s.charCodeAt(i)^k.charCodeAt(i%k.length));eval(d);})();`;
-
-    fs.writeFileSync('Dark Commands.obf.user.js', meta + '\n' + loader);
+    fs.writeFileSync('Dark Commands.obf.user.js', meta + '\n' + result.code);
     const kb = (fs.statSync('Dark Commands.obf.user.js').size / 1024).toFixed(1);
     console.log(`✓ Obfuscated: ${kb} KB`);
 }
