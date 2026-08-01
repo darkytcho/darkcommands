63dd7e5354164766965cda220d63f13840cd621f
Vítor Sousa <vitortls93@gmail.com>
2026-08-01 09:49:10 -0300

    v1.5.0: loader baixa sempre da release; build deterministico (seed) e eol=lf nos dist

--- build.js
+++ build.js
@@ -9,7 +9,8 @@ const OUT_OBFUSCATED = path.join(__dirname, 'dist', 'darkcommands.js');
 const OUT_LOADER = path.join(__dirname, 'dist', 'DarkCommands.obs.user.js');
 const TEMP_LOADER = path.join(__dirname, 'dist', '_loader_temp.js');
 
-const GITHUB_RAW = 'https://raw.githubusercontent.com/darkytcho/darkcommands/main/dist/darkcommands.js';
+const version = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version;
+const GITHUB_RAW = 'https://github.com/darkytcho/darkcommands/releases/download/v' + version + '/darkcommands.js';
 
 console.log('[build] Lendo source...');
 const source = fs.readFileSync(SRC, 'utf8');
@@ -27,7 +28,6 @@ const hash = crypto.createHash('sha256').update(obfuscated).digest('hex');
 console.log('[build] Hash:', hash);
 
 console.log('[build] Gerando loader...');
-const version = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version;
 
 const loaderCode = `(function () {
 	'use strict';
